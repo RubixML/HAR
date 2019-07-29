467cc25e9ba78b4085cbde5e9f03c937f849fec1
Andrew DalPino <me@andrewdalpino.com>
2019-07-28 22:34:30 -0500

    Update to Rubix ML 0.0.14-beta

--- explore.php
+++ explore.php
@@ -5,6 +5,7 @@ include __DIR__ . '/vendor/autoload.php';
 use Rubix\ML\Embedders\TSNE;
 use Rubix\ML\Datasets\Labeled;
 use Rubix\ML\Other\Loggers\Screen;
+use Rubix\ML\Transformers\ZScaleStandardizer;
 use Rubix\ML\Transformers\SparseRandomProjector;
 use Rubix\ML\Transformers\NumericStringConverter;
 use League\Csv\Reader;
@@ -32,16 +33,16 @@ $labels = Reader::createFromPath(__DIR__ . '/train/y_train.csv')
 $dataset = Labeled::fromIterator($samples, $labels)->randomize()->head(1000);
 
 $dataset->apply(new NumericStringConverter());
-$dataset->apply(new SparseRandomProjector(120));
+$dataset->apply(new ZScaleStandardizer());
 
-$embedder = new TSNE(2, 30, 12., 100);
+$embedder = new TSNE(2, 30, 12.0, 10.0);
 
 $embedder->setLogger(new Screen('HAR'));
 
-$embedding = $embedder->embed($dataset);
+$samples = $embedder->embed($dataset);
 
 $writer = Writer::createFromPath(OUTPUT_FILE, 'w+');
-$writer->insertOne(['x', 'y', 'label']);
-$writer->insertAll(iterator_to_array($embedding->zip()));
+$writer->insertOne(['x', 'y']);
+$writer->insertAll($samples);
 
 echo 'Embedding saved to ' . OUTPUT_FILE . PHP_EOL;
