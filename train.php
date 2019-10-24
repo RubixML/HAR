b922cb0be99ff4f4a520fc1a9dbd9b0dad61a5a5
Andrew DalPino <me@andrewdalpino.com>
2019-10-24 18:30:28 -0500

    Version bump to 0.0.16-beta

--- train.php
+++ train.php
@@ -15,6 +15,8 @@ use Rubix\ML\Other\Loggers\Screen;
 use League\Csv\Reader;
 use League\Csv\Writer;
 
+use function Rubix\ML\array_transpose;
+
 ini_set('memory_limit', '-1');
 
 echo 'Loading data into memory ...' . PHP_EOL;
@@ -44,7 +46,7 @@ $losses = $estimator->steps();
 
 $writer = Writer::createFromPath('progress.csv', 'w+');
 $writer->insertOne(['loss']);
-$writer->insertAll(array_map(null, $losses, []));
+$writer->insertAll(array_transpose([$losses]));
 
 echo 'Progress saved to progress.csv' . PHP_EOL;
 
